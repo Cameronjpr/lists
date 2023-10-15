0fd14e21ae07620777d865069d658fd79c5f560e
cameronjpr <cameronjpr@gmail.com>
2023-10-15 21:46:56 +0100

    move api

--- apps/extension/popup.js
+++ apps/extension/popup.js
@@ -1,3 +1,17 @@
-document.getElementById('addItem').addEventListener('click', function () {
+document.getElementById('addItem').addEventListener('click', async function () {
   console.log('This is a popup!')
+
+  const res = await fetch('https://lists-web.vercel.app/api/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      url: 'https://www.google.com',
+    }),
+  })
+
+  const data = await res.json()
+
+  console.log(data)
 })
